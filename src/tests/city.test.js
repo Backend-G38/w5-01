2a84c0b01a0791272ea84dfb2db0067faf592c6d
Gabriel Martinez <martinezgabrielalejandro4@gmail.com>
2024-08-15 23:12:09 -0300

    advancdes

--- src/tests/city.test.js
+++ src/tests/city.test.js
@@ -3,8 +3,8 @@ const app = require('../app')
 
 let cityId
 
-
 const BASE_URL = '/api/v1/cities'
+
 const city = {
   name: "Cordoba",
   country: "Arg",
@@ -40,3 +40,36 @@ test("GET -> BASE_URL, should return statusCode 200, and res.body.length === 1",
 //!!!!!!!!!!!!!! rutas dinamicas
 // /api/v1/cities/:ID
 //? BASE_URL     V.DINAMICO
+
+test("GET -> BASE_URL/cityId, should return statusCode 200, and res.body.city === city.name", async () => {
+
+  const res = await request(app)
+    .get(`${BASE_URL}/${cityId}`)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.name).toBe(city.name)
+})
+
+
+test("PUT -> BASE_URL/cityId, should return statusCode 200, and res.body.city === cityUpdate.name", async () => {
+
+  const cityUpdate = {
+    name: "Lima"
+  }
+
+  const res = await request(app)
+    .put(`${BASE_URL}/${cityId}`)
+    .send(cityUpdate)
+
+  expect(res.status).toBe(200)
+  expect(res.body).toBeDefined()
+  expect(res.body.name).toBe(cityUpdate.name)
+})
+
+test("Delete -> BASE_URL/cityId, should return statusCode 204", async () => {
+  const res = await request(app)
+    .delete(`${BASE_URL}/${cityId}`)
+
+  expect(res.statusCode).toBe(204)
+})
